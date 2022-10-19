dcf31843cf7abc2cb669a37b83ab15dce0186466
Emma-Jane MacKinnon-Lee <emma.jane@digitalax.xyz>
2022-10-19 10:27:49 -0400

    add loader

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,9 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { useEffect, useMemo, useRef } from "react";
+import { useMemo } from "react";
 import Footer from "../components/layout/Footer";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const isMounted = useRef(false);
   useMemo(() => {
     console.log(` **                                                                 
     /**                                                                 
@@ -50,26 +49,13 @@ function MyApp({ Component, pageProps }: AppProps) {
       const loader = document.getElementById("globalLoader");
       if (loader) loader.remove();
     }
-    console.log(isMounted)
-    isMounted.current = true;
-    console.log(isMounted)
-    return () => {
-      isMounted.current = false;
-    };
   }, []);
 
   return (
-    <>
-      {
-        isMounted ? <div className="min-h-auto h-auto min-w-screen w-screen bg-black relative cursor-empire selection:bg-lightYellow selection:text-lightYellow">
+    <div className="min-h-auto h-auto min-w-screen w-screen bg-black relative cursor-empire selection:bg-lightYellow selection:text-lightYellow">
       <Component {...pageProps} />
       <Footer />
-    </div> : <div>
-        heyooooo
     </div>
-      }
-    </>
-    
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,30 +8,49 @@ import Poster from "../components/home/poster/Poster";
 import Clear from "../components/home/clear/Clear";
 import Slider from "../components/home/slider/Slider";
 import Gap from "../components/home/gap/Gap";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 const Home: NextPage = (): JSX.Element => {
   const shopping = useRef<null | HTMLDivElement>(null);
   const goShopping = (): void => {
     shopping.current?.scrollIntoView({ behavior: "smooth" });
   };
+  const isMounted = useRef<boolean>(false);
+
+  useEffect(() => {
+    console.log(isMounted);
+    isMounted.current = true;
+    console.log(isMounted);
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
 
   return (
-    <div id="cursor" className="flex flex-col min-h-screen h-fit min-w-screen">
-      <Head>
-        <title>F3Manifesto</title>
-        <meta name="description" content="CC0 Web3 Fashion" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <VintageFilm />
-      <F3Manifesto />
-      <Web3Fashion goShopping={goShopping} />
-      <Collections shopping={shopping} />
-      <Poster />
-      <Clear />
-      <Slider />
-      <Gap />
-    </div>
+    <>
+      {isMounted ? (
+        <div
+          id="cursor"
+          className="flex flex-col min-h-screen h-fit min-w-screen"
+        >
+          <Head>
+            <title>F3Manifesto</title>
+            <meta name="description" content="CC0 Web3 Fashion" />
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+          <VintageFilm />
+          <F3Manifesto />
+          <Web3Fashion goShopping={goShopping} />
+          <Collections shopping={shopping} />
+          <Poster />
+          <Clear />
+          <Slider />
+          <Gap />
+        </div>
+      ) : (
+        <div className="h-screen min-h-screen w-full min-w-full bg-offBlack"></div>
+      )}
+    </>
   );
 };
 
