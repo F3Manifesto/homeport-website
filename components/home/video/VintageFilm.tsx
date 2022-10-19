9dc9243f7453e2a7f0a1d45f4b71befc8c26cf2c
Emma-Jane MacKinnon-Lee <emma.jane@digitalax.xyz>
2022-10-19 10:36:39 -0400

    add loader

--- components/home/video/VintageFilm.tsx
+++ components/home/video/VintageFilm.tsx
@@ -1,25 +1,42 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useEffect, useRef } from "react";
 import Sidebar from "../../layout/Sidebar";
 import Shutter from "./Shutter";
 
 const VintageFilm: FunctionComponent = (): JSX.Element => {
+  const isMounted = useRef<boolean>(false);
+
+  useEffect(() => {
+    console.log(isMounted);
+    isMounted.current = true;
+    console.log(isMounted);
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
+
   return (
-    <div className="max-h-screen h-full min-w-full w-full relative block clear-both overflow-hidden">
-      <Sidebar />
-      <div className="relative min-h-full h-full w-full">
-        <video
-          className="relative h-full min-h-full object-cover"
-          autoPlay
-          muted
-          loop
-        >
-          <source src="/videos/homevideo.mp4" type="video/mp4" />
-        </video>
-        <div className="absolute top-0 left-0 hidden sm:flex">
-          <Shutter />
+    <>
+      {isMounted ? (
+        <div className="min-h-full h-full min-w-full w-full relative block clear-both overflow-hidden">
+          <Sidebar />
+          <div className="relative min-h-full h-full w-full">
+            <video
+              className="relative h-full min-h-full object-cover"
+              autoPlay
+              muted
+              loop
+            >
+              <source src="/videos/homevideo.mp4" type="video/mp4" />
+            </video>
+            <div className="absolute top-0 left-0 hidden sm:flex">
+              <Shutter />
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      ) : (
+        <div className="min-h-screen h-screen bg-offBlack min-w-full w-full relative block clear-both overflow-hidden"></div>
+      )}
+    </>
   );
 };
 
