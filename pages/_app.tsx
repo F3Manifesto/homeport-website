8eb2e3a8c2aa336e1bd2c744ee9c38b7597fab54
Emma-Jane MacKinnon-Lee <emma.jane@digitalax.xyz>
2022-10-19 09:33:24 -0400

    full responsivitiy across app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { useMemo } from "react";
+import { useEffect, useMemo } from "react";
 import Footer from "../components/layout/Footer";
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -48,15 +48,19 @@ function MyApp({ Component, pageProps }: AppProps) {
   }, []);
   const componentDidMount = () => {
     const removeFouc = (foucElement: any) => {
-      foucElement.className = foucElement.className.replace('no-fouc', 'fouc');
+      foucElement.className = foucElement.className.replace("no-fouc", "fouc");
     };
-  
+
     removeFouc(document.documentElement);
-  }
+  };
+
+  useEffect(() => {
+    componentDidMount();
+  });
   return (
     <div className="min-h-auto h-auto min-w-screen w-screen bg-black relative cursor-empire selection:bg-lightYellow selection:text-lightYellow">
-        <Component {...pageProps} />
-        <Footer />
+      <Component {...pageProps} />
+      <Footer />
     </div>
   );
 }
