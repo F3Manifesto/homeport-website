3be679ae4f9567e72d692d31ad1bc3822bc0fb1c
Emma-Jane MacKinnon-Lee <emma.jane@digitalax.xyz>
2022-10-19 22:21:10 -0400

    media optimisation & banner

--- components/home/video/VintageFilm.tsx
+++ components/home/video/VintageFilm.tsx
@@ -13,7 +13,7 @@ const VintageFilm: FunctionComponent = (): JSX.Element => {
       id="lanvideo"
     >
       {showBanner && (
-        <div className="bg-offBlack text-offWhite font-firaL font-sm p-4">
+        <div className="bg-offBlack text-offWhite font-firaL text-xs sm:text-sm p-4 ease-out duration-1000">
           Your signal may not be strong enough to load this interface all at
           once.
           <br />
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,20 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { Suspense, useEffect } from "react";
+import { Suspense, useEffect, useState, createContext } from "react";
 import Footer from "../components/layout/Footer";
 import Loading from "../components/layout/Loading";
 
+export const GlobalProfileContextDefault = {
+  orderIRL: "",
+  setOrderIRL: (orderIRL: string) => {},
+};
+
+export const GlobalContext = createContext(GlobalProfileContextDefault);
+
 function MyApp({ Component, pageProps }: AppProps) {
+  const [orderIRL, setOrderIRL] = useState(
+    GlobalProfileContextDefault.orderIRL
+  );
   useEffect(() => {
     console.log(` **                                                                 
     /**                                                                 
@@ -50,10 +60,12 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <Suspense fallback={Loading}>
-      <div className="min-h-screen h-auto min-w-screen w-screen bg-black relative cursor-empire selection:bg-lightYellow selection:text-lightYellow">
-        <Component {...pageProps} />
-        <Footer />
-      </div>
+      <GlobalContext.Provider value={{ orderIRL, setOrderIRL }}>
+        <div className="min-h-screen h-auto min-w-screen w-screen bg-black relative cursor-empire selection:bg-lightYellow selection:text-lightYellow">
+          <Component {...pageProps} />
+          <Footer />
+        </div>
+      </GlobalContext.Provider>
     </Suspense>
   );
 }
