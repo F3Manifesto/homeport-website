839192ae0c2eead34e6b501e9f942e961f7e9653
emmajane1313 <admin@digitalax.xyz>
2022-10-23 04:19:46 -0400

    minor: slider height

--- components/home/slider/Slider.tsx
+++ components/home/slider/Slider.tsx
@@ -39,7 +39,7 @@ const Slider: FunctionComponent = (): JSX.Element => {
   ];
 
   return (
-    <div className="min-h-60 h-60 flex relative w-full bg-offWhite cursor-empireA overflow-hidden pb-10">
+    <div className="min-h-72 h-72 flex relative w-full bg-offWhite cursor-empireA overflow-hidden pb-10">
       <Marquee className="flex" pauseOnHover pauseOnClick direction="right">
         {images.map((image, key) => {
           return (
