2dfb61eb02a736ac572302a07e852092de9965d7
emmajane1313 <admin@digitalax.xyz>
2022-11-02 15:09:12 -0400

    add header collect

--- pages/collect/[name].tsx
+++ pages/collect/[name].tsx
@@ -9,7 +9,7 @@ import Connect from "../../components/common/connect/Connect";
 import Approve from "../../components/common/modals/Approve";
 import useMetadata from "../../components/collect/hooks/useMetadata";
 import { useAccount, useBalance } from "wagmi";
-import useApprove from "../../components/common/modals/hooks/useApprove";
+import Head from "next/head";
 
 export const CollectContextDefault = {
   showApprovalModal: false,
@@ -116,6 +116,36 @@ const TokenDetails = ({ token }: any): JSX.Element => {
       }}
     >
       <div className="flex min-h-screen h-fit min-w-screen relative cursor-empire selection:bg-lightYellow selection:text-lightYellow cursor-empireA bg-gradient-to-b from-lightY via-white to-lightPurple z-0">
+        <Head>
+          <meta name="og:url" content="https://f3manifesto.xyz/" />
+          <meta name="og:title" content={token[0].name} />
+          <meta name="og:description" content={token[0].description} />
+          <meta
+            name="og:image"
+            content={`https://f3manifesto.xyz/images/${token[0].image}`}
+          />
+          <meta name="twitter:card" content="summary" />
+          <meta name="og:url" content="https://f3manifesto.xyz/" />
+          <meta
+            name="og:image"
+            content={`https://f3manifesto.xyz/images/${token[0].image}`}
+          />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="@f3manifesto" />
+          <meta name="twitter:creator" content="@f3manifesto" />
+          <meta
+            name="twitter:image"
+            content={`https://f3manifesto.xyz/images/${token[0].image}`}
+          />
+          <meta name="twitter:url" content="https://f3manifesto.xyz/" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="canonical" href="https://f3manifesto.xyz/" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+        </Head>
         {showApprovalModal && (
           <div
             className={`${
