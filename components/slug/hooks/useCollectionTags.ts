63b73fa0f61e74e10b608ab84e2a90c3d16f1eb4
Emma-Jane MacKinnon-Lee <emma.jane@digitalax.xyz>
2022-11-11 15:17:52 -0500

    rerun gitignore

--- components/slug/hooks/useCollectionTags.ts
+++ components/slug/hooks/useCollectionTags.ts
@@ -1,4 +1,4 @@
-import { useState, useMemo, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { UseCollectionTagsResult } from "../../../types/general.types";
 import arrayShuffle from "array-shuffle";
 
