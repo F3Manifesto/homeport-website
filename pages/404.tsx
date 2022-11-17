24a637f51c5870761a0427a70a02db89eeba4885
Emma-Jane MacKinnon-Lee <emma.jane@digitalax.xyz>
2022-11-17 08:19:55 -0500

    &apos;

--- pages/404.tsx
+++ pages/404.tsx
@@ -5,7 +5,7 @@ const Custom404: NextPage = (): JSX.Element => {
   return (
     <div className="relative min-h-screen min-w-screen h-screen w-screen grid grid-flow-col auto-cols-[auto auto] bg-shaded">
       <div className="relative w-fit h-fit place-self-center text-yellowTheme font-economica p-6 text-center text-3xl">
-        There's been a glitch in the fabric. Find your way back{" "}
+        There&apos;s been a glitch in the fabric. Find your way back{" "}
         <Link legacyBehavior href="/">
           <a className="hover:opacity-80 cursor-pointer">home.</a>
         </Link>
