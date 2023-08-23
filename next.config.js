06b34910918524a8d3271750bfc9fb30b33a4695
Emma-Jane MacKinnon-Lee <emma.jane@digitalax.xyz>
2023-08-23 19:11:05 -0400

    update pqts + links

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
     scrollRestoration: true,
     optimizeCss: true,
   },
+
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
