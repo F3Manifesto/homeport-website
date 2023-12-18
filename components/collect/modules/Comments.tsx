d5441e53d4b0c2cd640ac3aa6d981d84bbbdb1f2
emmajane1313 <admin@digitalax.xyz>
2023-12-18 16:06:23 -0500

    react node

--- components/collect/modules/Comments.tsx
+++ components/collect/modules/Comments.tsx
@@ -97,71 +97,73 @@ const Comments: FunctionComponent<CommentProps> = ({
             No comments yet. Make one?
           </div>
         ) : (
-          <InfiniteScroll
-            dataLength={comments?.length}
-            loader={<></>}
-            hasMore={commentInfo?.hasMore}
-            next={getMoreComments}
-            className="w-full h-fit items-center justify-start flex flex-col gap-10"
-          >
-            {comments?.map((item: Comment, index: number) => {
-              return (
-                <Publication
-                  main={false}
-                  lensConnected={lensConnected}
-                  index={index}
-                  item={item}
-                  dispatch={dispatch}
-                  data-post-id={item?.id}
-                  key={index}
-                  top={
-                    item?.metadata?.content?.length < 100 &&
-                    item?.metadata?.__typename !== "AudioMetadataV3" &&
-                    item?.metadata?.__typename !== "ImageMetadataV3" &&
-                    item?.metadata?.__typename !== "VideoMetadataV3"
-                      ? "20px"
-                      : "auto"
-                  }
-                  bottom={
-                    item?.metadata?.content?.length < 100 &&
-                    item?.metadata?.__typename !== "AudioMetadataV3" &&
-                    item?.metadata?.__typename !== "ImageMetadataV3" &&
-                    item?.metadata?.__typename !== "VideoMetadataV3"
-                      ? "auto"
-                      : "2px"
-                  }
-                  left={"auto"}
-                  right={"2px"}
-                  mirror={mirror}
-                  comment={comment}
-                  like={like}
-                  interactionsLoading={interactionsLoading}
-                  profileHovers={profileHovers}
-                  setProfileHovers={setProfileHovers}
-                  openMirrorChoice={openMirrorChoice}
-                  setOpenMirrorChoice={setOpenMirrorChoice}
-                  unfollowProfile={unfollowProfile}
-                  simpleCollect={simpleCollect}
-                  followLoading={followLoading}
-                  followProfile={followProfile}
-                  commentsOpen={commentsOpen}
-                  setCommentsOpen={setCommentsOpen}
-                  makeComment={makeComment}
-                  setMakePostComment={setMakeComment}
-                  setContentLoading={setContentLoading}
-                  contentLoading={contentLoading}
-                  postCollect={postCollect}
-                  setMentionProfiles={setMentionProfiles}
-                  setProfilesOpen={setProfilesOpen}
-                  mentionProfiles={mentionProfiles}
-                  profilesOpen={profilesOpen}
-                  caretCoord={caretCoord}
-                  setCaretCoord={setCaretCoord}
-                  quote={false}
-                />
-              );
-            })}
-          </InfiniteScroll>
+          <>
+            <InfiniteScroll
+              dataLength={comments?.length}
+              loader={<></>}
+              hasMore={commentInfo?.hasMore}
+              next={getMoreComments}
+              className="w-full h-fit items-center justify-start flex flex-col gap-10"
+            >
+              {comments?.map((item: Comment, index: number) => {
+                return (
+                  <Publication
+                    main={false}
+                    lensConnected={lensConnected}
+                    index={index}
+                    item={item}
+                    dispatch={dispatch}
+                    data-post-id={item?.id}
+                    key={index}
+                    top={
+                      item?.metadata?.content?.length < 100 &&
+                      item?.metadata?.__typename !== "AudioMetadataV3" &&
+                      item?.metadata?.__typename !== "ImageMetadataV3" &&
+                      item?.metadata?.__typename !== "VideoMetadataV3"
+                        ? "20px"
+                        : "auto"
+                    }
+                    bottom={
+                      item?.metadata?.content?.length < 100 &&
+                      item?.metadata?.__typename !== "AudioMetadataV3" &&
+                      item?.metadata?.__typename !== "ImageMetadataV3" &&
+                      item?.metadata?.__typename !== "VideoMetadataV3"
+                        ? "auto"
+                        : "2px"
+                    }
+                    left={"auto"}
+                    right={"2px"}
+                    mirror={mirror}
+                    comment={comment}
+                    like={like}
+                    interactionsLoading={interactionsLoading}
+                    profileHovers={profileHovers}
+                    setProfileHovers={setProfileHovers}
+                    openMirrorChoice={openMirrorChoice}
+                    setOpenMirrorChoice={setOpenMirrorChoice}
+                    unfollowProfile={unfollowProfile}
+                    simpleCollect={simpleCollect}
+                    followLoading={followLoading}
+                    followProfile={followProfile}
+                    commentsOpen={commentsOpen}
+                    setCommentsOpen={setCommentsOpen}
+                    makeComment={makeComment}
+                    setMakePostComment={setMakeComment}
+                    setContentLoading={setContentLoading}
+                    contentLoading={contentLoading}
+                    postCollect={postCollect}
+                    setMentionProfiles={setMentionProfiles}
+                    setProfilesOpen={setProfilesOpen}
+                    mentionProfiles={mentionProfiles}
+                    profilesOpen={profilesOpen}
+                    caretCoord={caretCoord}
+                    setCaretCoord={setCaretCoord}
+                    quote={false}
+                  />
+                );
+              })}
+            </InfiniteScroll>
+          </>
         )}
       </div>
     </div>
