96339b7baea3b8c0cb777d723bccf15aa61b8807
Emma-Jane MacKinnon-Lee <emma.jane@digitalax.xyz>
2024-01-05 05:04:16 -0500

    module currency

--- graphql/lens/mutations/approve.ts
+++ graphql/lens/mutations/approve.ts
@@ -9,8 +9,8 @@ import {
 const approveCurrency = async (
   request: GenerateModuleCurrencyApprovalDataRequest
 ): Promise<FetchResult<GenerateModuleCurrencyApprovalDataQuery>> => {
-  return await apolloClient.query({
-    query: GenerateModuleCurrencyApprovalDataDocument,
+  return await apolloClient.mutate({
+    mutation: GenerateModuleCurrencyApprovalDataDocument,
     variables: {
       request,
     },
