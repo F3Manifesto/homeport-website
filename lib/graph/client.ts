a0e5fe3ca976c7f70e27ad9da4a7551440057039
emmajane1313 <admin@digitalax.xyz>
2024-05-13 22:49:25 -0400

    graph se actualizo

--- lib/graph/client.ts
+++ lib/graph/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 const httpLinkPrint = new HttpLink({
-  uri: "https://api.thegraph.com/subgraphs/name/digitalax/print-library",
+  uri: `https://gateway-arbitrum.network.thegraph.com/api/${process.env.NEXT_PUBLIC_GRAPH_KEY}/subgraphs/id/DcuUkg3QC5zg1t86VeNjWzg6R6ohaGa8QGyVE1rFYMZB`,
 });
 
 export const graphPrintClient = new ApolloClient({
